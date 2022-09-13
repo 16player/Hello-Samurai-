c4bddd9ede6bdd3bcced62f5a6e8eaae267cb9e9
16player <191292km@gmail.com>
2022-09-13 22:11:18 +0300

    update

---
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+
+
+const MyPosts = () => {
+    return <div className={s.content}>
+        <div>
+            <img alt=''
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYPLDCsQbyInk-C5TZAlgwKO34mFlMOSssLw&usqp=CAU'/>
+        </div>
+        <div >
+            ava + description
+        </div>
+        <div >
+            My posts
+        </div>
+        <div >
+            New post
+        </div>
+        <div className={s.posts}>
+            <div className={s.item}>
+                Post 1
+            </div>
+            <div className={s.item}>
+                Post2
+            </div>
+        </div>
+    </div>
+}
+
+export default MyPosts;
