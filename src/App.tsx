f64c703c196a12a40b7cabb5d503d9e43a0e13e1
16player <191292km@gmail.com>
2022-09-21 10:39:24 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import './App.css';
 import Profile from "./components/Profile/Profile";
 import Header from './components/Header/Header';
 import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
-    return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Profile/>
-            <Navbar/>
-        </div>)
+    return (<BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                {/*<Profile/>*/}
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
